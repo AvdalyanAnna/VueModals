b45753679f99448b2b7a1d921a5194c39f29caa1
AvdalyanAnna <anna.avdalyan.2017@gmail.com>
2021-06-08 00:44:32 +0400

    add first and second modals

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# VueModals
+# Vue Modals
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,81 @@
 <template>
-  <div class="wrappeer">
-<!--    <header></header>-->
-  <div class="wrapper-content">
-    <section>
-      <div class="container">
-        <h1>Hello</h1>
-        <img alt="Vue logo" src="./assets/logo.png">
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
 
-      </div>
-    </section>
-  </div>
-<!--  <footer></footer>-->
+          <!-- first modal -->
+          <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
+          <modals
+              title="First Modal"
+              v-show="modalFirst"
+              @close="modalFirst = false"
+          >
+            <!-- body -->
+            <div slot="body">
+              <p>Text Text Text Text Text Text Text Text Text </p>
+              <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Well Done!</button>
+            </div>
+          </modals>
+
+          <!-- second modal -->
+          <button class="btn btnPrimary" @click="modalSecond.show = !modalSecond.show">Show modal with form</button>
+          <modals
+              title="Modal with form"
+              v-show="modalSecond.show"
+              @close="modalSecond.show = false"
+          >
+            <!-- body -->
+            <div slot="body">
+              <form @submit.prevent="submitSecondForm">
+                <label>Name:</label>
+                <input type="text" required v-model="modalSecond.name">
+                <label>Email:</label>
+                <input type="email" required v-model="modalSecond.email">
+                <button type="submit" class="btn btnPrimary">Submit!</button>
+
+              </form>
+            </div>
+          </modals>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+import Modals from "@/components/Modal";
 
 export default {
   name: 'App',
   components: {
+    Modals
+  },
+  data() {
+    return {
+      modalFirst: false,
+      modalSecond: {
+        show: false,
+        name: '',
+        email: '',
+      }
+    }
+  },
+  methods: {
+    submitSecondForm() {
+      console.log({
+        name: this.modalSecond.name,
+        email: this.modalSecond.email,
+      })
+      this.modalSecond.name = '';
+      this.modalSecond.email = '';
+      this.modalSecond.show = false;
+    }
   }
+
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
 </style>
